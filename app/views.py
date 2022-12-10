eedd0d246b62cd8f7a350b333f0407c9a4681ebc
Towareesh <63032856+Towareesh@users.noreply.github.com>
2022-12-10 13:19:22 +0200

    init

---
+++ app/views.py
@@ -0,0 +1,64 @@
+from flask import (render_template,
+                   redirect,
+                   url_for,
+                   flash)
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index/')
+@app.route('/index')
+def index():
+    """ Decorated func: index()
+        Представление страницы с моделью микроблога.
+        Обрабатываемый шаблон index.html наследуется от base.html.
+
+    Returns:
+        str: object render_template
+    """
+
+    user = {'username': 'Аннушка'}
+    posts = [
+        {
+            'author': {'username': 'Vlad'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Tony'},
+            'body': 'Nice mother'
+        },
+        {
+            'author': {'username': 'Mark'},
+            'body': 'Bullshit!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/home')
+def home():
+    """ Decorated func: home() 
+        Представление главной страницы.
+
+    Returns:
+        str: object render_template
+    """
+
+    return render_template('base.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """ Decorated func: login()
+        Представление страницы с моделью регистации.
+        Обрабатываемый шаблон login.html наследуется от base.html.
+
+    Returns:
+        str: object render_template
+    """
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Войти', form=form)
---
+++ runner.py
@@ -0,0 +1,13 @@
+import datetime
+import os
+
+from app import app
+
+now = datetime.datetime.now()
+log = ['worked: runner.py', f'[{now.strftime("%d-%m-%Y %H:%M")}]']
+
+if __name__ == '__main__':
+    
+    print(f'\u001b[34m{log[1]}\u001b[0m\u001b[32m {log[0]}\u001b[0m\n')
+    app.debug = True
+    app.run(host='0.0.0.0')
